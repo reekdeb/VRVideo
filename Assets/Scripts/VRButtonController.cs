262a947bafaddc241c0cff8cff3aac070870379a
Reekdeb Mal <reekdeb@gmail.com>
2021-03-26 23:23:08 +0530

    Add UI scripts

---
+++ Assets/Scripts/VRButtonController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Animator))]
+public class VRButtonController : MonoBehaviour
+{
+    [SerializeField]
+    private UnityEvent OnClick;
+
+    private Animator animator;
+    private int onGaze = Animator.StringToHash("OnGaze");
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void OnPointerEnter()
+    {
+        animator.SetBool(onGaze, true);
+    }
+
+    public void OnPointerExit()
+    {
+        animator.SetBool(onGaze, false);
+    }
+
+    public void OnPointerClick()
+    {
+        OnClick.Invoke();
+    }
+}
---
+++ Assets/Scripts/VRUIManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+
+public class VRUIManager : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField]
+    private VideoPlayer videoPlayer;
+    [SerializeField]
+    private SpriteRenderer playPauseButton;
+
+    [Header("Assets")]
+    [SerializeField]
+    private Sprite videoPlaySprite;
+    [SerializeField]
+    private Sprite videoPauseSprite;
+
+    public void PlayPauseToggle()
+    {
+        if (videoPlayer.isPlaying)
+        {
+            videoPlayer.Pause();
+            playPauseButton.sprite = videoPlaySprite;
+        }
+        else
+        {
+            videoPlayer.Play();
+            videoPlayer.GetComponent<MeshRenderer>().enabled = true;
+            playPauseButton.sprite = videoPauseSprite;
+        }
+    }
+}
